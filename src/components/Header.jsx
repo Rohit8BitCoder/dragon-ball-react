4409251be2d6fd1e0db841d2dafd7f17d5689381
Rohit8BitCoder <princerohit2002@gmail.com>
2025-06-26 14:58:45 +0530

    changed usenavigate to navLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,35 @@
 import logo2 from '../assets/logo2.png';
-import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-
-
-export default function Header () {
-const navigate = useNavigate();
-  const HandleClickChar = () => {
-    navigate('/'); 
-  };
-
-  const HandleClickPlanets = () => {
-    navigate('/planets'); 
-  };
-  
+export default function Header() {
   return (
     <div className='flex justify-between items-center'>
       <div className='flex ps-2'>
-        <img src={logo2} alt='logo' width="200" height="100"/>
+        <img src={logo2} alt='logo' width="150" height="100" />
       </div>
       <div className='flex gap-2 pr-4'>
-        <button 
-        onClick={HandleClickChar}
-        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"> Characters </button>
-        
-        <button 
-        onClick={HandleClickPlanets}
-        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"> Planets</button>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `bg-white font-semibold py-2 px-4 border border-gray-400 rounded shadow 
+             hover:bg-gray-100 text-gray-800 
+             ${isActive ? "text-orange-700 border-orange-400" : ""}`
+          }
+        >
+          Characters
+        </NavLink>
+
+        <NavLink
+          to="/planets"
+          className={({ isActive }) =>
+            `bg-white font-semibold py-2 px-4 border border-gray-400 rounded shadow 
+             hover:bg-gray-100 text-gray-800 
+             ${isActive ? "text-orange-700 border-orange-400" : ""}`
+          }
+        >
+          Planets
+        </NavLink>
       </div>
-    
     </div>
-  )
+  );
 }
